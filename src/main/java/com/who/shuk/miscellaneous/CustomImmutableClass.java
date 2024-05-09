0bab57e2a426eb864e3a213e38075092fdf6ba8c
shukwho <shsushmita95@gmail.com>
2024-05-09 14:56:29 +0530

    Collection field changes

--- src/main/java/com/who/shuk/miscellaneous/CustomImmutableClass.java
+++ src/main/java/com/who/shuk/miscellaneous/CustomImmutableClass.java
@@ -2,18 +2,28 @@ package com.who.shuk.miscellaneous;
 
 import com.who.shuk.exceptionHandling.CustomException;
 
+import java.util.ArrayList;
+import java.util.Collections;
 import java.util.Date;
+import java.util.List;
 
 public final class CustomImmutableClass {
 
     private final int field1;
     private final String field2;
 
+    private final Date date;
 
-    public CustomImmutableClass(int field1, String field2, Date date) {
+
+
+    private final List<String> listField;
+
+
+    public CustomImmutableClass(int field1, String field2, Date date, List<String> listField) {
         this.field1 = field1;
         this.field2 = field2;
         this.date = date;
+        this.listField = listField;
     }
 
     public int getField1() {
@@ -29,7 +39,10 @@ public final class CustomImmutableClass {
         return (Date) date.clone();
     }
 
-    private final Date date;
+    public List<String> getListField() {
+        return new ArrayList<>(listField);
+    }
+
 
     @Override
     public String toString() {
@@ -37,6 +50,7 @@ public final class CustomImmutableClass {
                 "field1=" + field1 +
                 ", field2='" + field2 + '\'' +
                 ", date=" + date +
+                ", listField=" + listField +
                 '}';
     }
 }
@@ -45,7 +59,8 @@ class Test {
     public static void main(String[] args) {
         //CustomImmutableClass customImmutableClass = new CustomImmutableClass(1,"field2");
         //customImmutableClass.field2="field2updated"; -- this line throws error as we can't change immutable class
-        CustomImmutableClass customImmutableClass = new CustomImmutableClass(1, "field2", new Date());
+        //CustomImmutableClass customImmutableClass = new CustomImmutableClass(1, "field2", new Date());
+        CustomImmutableClass customImmutableClass = new CustomImmutableClass(1,"field2", new Date(), Collections.singletonList("list"));
         System.out.println(customImmutableClass);
         customImmutableClass.getDate().setDate(15);
         System.out.println(customImmutableClass);
