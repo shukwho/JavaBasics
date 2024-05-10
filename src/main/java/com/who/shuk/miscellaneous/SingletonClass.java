6770a2ce1d0889696aa0ca0a711352deafc784b8
shukwho <shsushmita95@gmail.com>
2024-05-10 14:50:32 +0530

    SingletonClass

---
+++ src/main/java/com/who/shuk/miscellaneous/SingletonClass.java
@@ -0,0 +1,21 @@
+package com.who.shuk.miscellaneous;
+
+public class SingletonClass {
+    private static SingletonClass singletonClasInstance=null;
+    private SingletonClass(){
+
+    }
+    public static SingletonClass getInstance(){
+        if(singletonClasInstance ==null){
+            singletonClasInstance = new SingletonClass();
+        }
+        return singletonClasInstance;
+    }
+}
+class TestSingleton{
+    public static void main(String[] args) {
+        SingletonClass s1 = SingletonClass.getInstance();
+        SingletonClass s2 = SingletonClass.getInstance();
+        System.out.println(s1.hashCode()+" "+s2.hashCode());
+    }
+}
